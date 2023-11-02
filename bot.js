67e3d3cbea0bd9c14c7114e66e1655c96ef41d73
nautilo <8lapse@gmail.com>
2023-11-02 11:41:13 -0300

    updated bot render qr in webpage

--- bot.js
+++ bot.js
@@ -1,30 +1,46 @@
 const express = require('express');
-const qrcode = require('qrcode'); // Asumiendo que estás utilizando la biblioteca 'qrcode' para generar códigos QR.
+const qrcode = require('qrcode-terminal');
+const { Client } = require('whatsapp-web.js');
 const app = express();
 
-// Lógica para generar el código QR
-const generateQRCode = async (text) => {
-  try {
-    return await qrcode.toDataURL(text);
-  } catch (error) {
-    console.error('Error generando el código QR:', error);
-    return null;
-  }
-}
+// Crear una instancia del cliente de WhatsApp Web
+const client = new Client();
 
-// Ruta para mostrar el código QR
-app.get('/qr', async (req, res) => {
-  // Genera el código QR con el contenido deseado (puede ser dinámico)
-  const qrCodeText = 'Hola, soy un bot de WhatsApp!';
-  const qrCodeDataURL = await generateQRCode(qrCodeText);
+// Cuando el cliente emite el evento "qr", generamos y mostramos el código QR
+client.on('qr', (qrCode) => {
+  // Renderiza el código QR en la consola
+  qrcode.generate(qrCode, { small: true });
+  console.log('Escanea el código QR con tu teléfono.');
 
-  // Muestra el código QR en la respuesta
-  if (qrCodeDataURL) {
-    res.type('image/png');
-    res.send(qrCodeDataURL);
-  } else {
-    res.status(500).send('Error generando el código QR');
+  // También puedes enviar el código QR al cliente web a través de una ruta
+  // En este ejemplo, podríamos usar una variable global o una base de datos temporal
+  // para almacenar el código QR y luego proporcionar una ruta para mostrarlo en la aplicación web.
+});
+
+// Cuando el cliente está listo, inicia el servidor Express
+client.on('ready', () => {
+  console.log('El bot está listo para recibir mensajes.');
+});
+
+// Escucha los mensajes entrantes
+client.on('message', async (message) => {
+  if (message.body.toLowerCase() === 'holabot') {
+    // Responder "Hola, soy un bot." al mensaje "holabot"
+    message.reply('Hola, soy un bot.');
+  } else if (message.body.toLowerCase() === 'chaobot') {
+    // Responder "Chao, soy un bot." al mensaje "chaobot"
+    message.reply('Chao, soy un bot.');
   }
+});
+
+// Inicializar el cliente de WhatsApp Web
+client.initialize();
+
+// Ruta para mostrar el código QR en la aplicación web
+app.get('/', (req, res) => {
+  // Puedes agregar aquí la lógica para mostrar el código QR en la respuesta
+  // Utiliza la misma lógica para mostrar el código QR almacenado anteriormente si es necesario
+  res.send('Aquí debería mostrarse el código QR.');
 });
 
 // Iniciar el servidor Express
