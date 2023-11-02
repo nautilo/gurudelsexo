89751a1cf19a068f2fe516a641aa1ddf2da44e58
nautilo <8lapse@gmail.com>
2023-11-02 12:01:45 -0300

    gurudelsexo fixed

--- app.js
+++ app.js
@@ -1,46 +1,44 @@
 const express = require('express');
-const qrcode = require('qrcode-terminal');
+const qr = require('qrcode'); // Utilizamos la biblioteca 'qrcode' para generar códigos QR.
 const { Client } = require('whatsapp-web.js');
 const app = express();
 
 // Crear una instancia del cliente de WhatsApp Web
 const client = new Client();
 
-let qrCodeData; // Variable para almacenar el código QR
+// Cuando el cliente emite el evento "qr", generamos y mostramos el código QR
+client.on('qr', async (qrCode) => {
+  // Genera el código QR como una imagen y proporciona una URL para mostrarlo
+  const qrCodeURL = await qr.toDataURL(qrCode);
 
-// Cuando el cliente emite el evento "qr", almacenamos el código QR
-client.on('qr', (qrCode) => {
-  qrCodeData = qrCode; // Almacenar el código QR en la variable
+  // Muestra el código QR en la respuesta de la ruta principal
+  app.get('/', (req, res) => {
+    res.send(`
+      <html>
+        <body>
+          <img src="${qrCodeURL}" alt="QR Code" />
+          <p>Escanea el código QR con tu teléfono para iniciar sesión en WhatsApp Web.</p>
+        </body>
+      </html>
+    `);
+  });
+
   console.log('Escanea el código QR con tu teléfono.');
 });
 
-// Cuando el cliente está listo, inicia el servidor Express
-client.on('ready', () => {
-  console.log('El bot está listo para recibir mensajes.');
-
-  // Iniciar el servidor Express y proporcionar el código QR en la ruta raíz
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => {
-    console.log(`Aplicación escuchando en el puerto ${port}`);
-  });
+// Manejador de mensajes entrantes
+client.on('message', (message) => {
+  const body = message.body.toLowerCase();
+  if (body === 'holabot') {
+    message.reply('Hola, soy un bot.');
+  } else if (body === 'chaobot') {
+    message.reply('Chao, soy un bot.');
+  }
 });
 
-// Inicializar el cliente de WhatsApp Web
-client.initialize();
-
-// Ruta para mostrar el código QR en la página web
-app.get('/', (req, res) => {
-  if (qrCodeData) {
-    // Genera el código QR en formato de imagen y envíalo como respuesta
-    qrcode.toDataURL(qrCodeData, (err, url) => {
-      if (err) {
-        console.error('Error generando el código QR:', err);
-        res.status(500).send('Error generando el código QR');
-      } else {
-        res.send(`<img src="${url}" alt="Código QR">`);
-      }
-    });
-  } else {
-    res.status(404).send('Código QR no disponible');
-  }
+// Iniciar el servidor Express
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Aplicación escuchando en el puerto ${port}`);
+  client.initialize(); // Inicializar el cliente de WhatsApp Web cuando el servidor esté listo.
 });
